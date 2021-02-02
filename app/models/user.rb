fcbb653d1177d81965eac643e5fc7acc52278767
kazuki <matchingserver@gmail.com>
2021-02-02 23:04:32 +0900

    define association and reset migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,17 +14,21 @@ ActiveRecord::Schema.define(version: 2021_01_31_161508) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
+    t.integer "user_id", null: false
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +40,6 @@ ActiveRecord::Schema.define(version: 2021_01_31_161508) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
