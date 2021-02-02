09847648ea41abaa0059d91f81e3cd0d0a1fa441
kazuki <matchingserver@gmail.com>
2021-02-02 20:23:36 +0900

    add user foreign key

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -3,6 +3,7 @@ module Types
     field :id, ID, null: false
     field :content, String, null: true
     field :post_id, Integer, null: false
+    field :user_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
